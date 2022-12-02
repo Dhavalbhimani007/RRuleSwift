59dcf818518ec968718d18296d8b3708521c4185
Dhaval <dhavaldbhimani4@gmail.com>
2022-12-02 17:11:39 +0530

    rename target

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "RRuleSwift",
+    name: "RRuleSwiftUphabit",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "RRuleSwift",
+            name: "RRuleSwiftUphabit",
             targets: ["RRuleSwift"]),
     ],
     dependencies: [
@@ -19,11 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "RRuleSwift",
+            name: "RRuleSwiftUphabit",
             dependencies: [],
-            path: "Sources"),
-        .testTarget(
-            name: "RRuleSwiftTests",
-            dependencies: ["RRuleSwift"]),
+            path: "Sources")
     ]
 )
